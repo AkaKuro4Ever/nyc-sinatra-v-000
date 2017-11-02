d1d9b6e921e5d9c0cc42d8e7e00a36acd005bff1
Deborah Seow <deborahseow9@gmail.com>
2017-11-02 16:00:53 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -3,7 +3,61 @@ class FiguresController < ApplicationController
   get '/figures/new' do
 
     erb :'/figures/new'
-    binding.pry
   end
 
+  get '/figures' do
+
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by(id: params[:id])
+
+    erb :'/figures/figure'
+  end
+
+  post '/figures' do
+    @figure = Figure.new(params[:figure])
+    if !params[:landmark][:name].empty?
+      if @landmark = Landmark.find_by(name: params[:landmark][:name])
+        @figure.landmarks << @landmark
+      else
+        @landmark = Landmark.create(name: params[:landmark][:name])
+        @figure.landmarks << @landmark
+      end
+    end
+      if !params[:title][:name].empty?
+        if @title = Title.find_by(name: params[:title][:name])
+          @title.figures << @figure
+        else
+          @title = Title.create(name: params[:title][:name])
+          @title.figures << @figure
+        end
+      end
+    @figure.save
+    @title.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by(id: params[:id])
+    erb :edit
+  end
+
+  patch '/figures/:id' do #edit action
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:id])
+    if !params[:landmark][:name].empty?
+      if @landmark = Landmark.find_by(name: params[:landmark][:name])
+        @figure.landmarks << @landmark
+      else
+        @landmark = Landmark.create(name: params[:landmark][:name])
+        @figure.landmarks << @landmark
+      end
+      @figure.save
+    end
+
+    @figure.save
+    redirect to "/figure/#{@figure.id}"
+  end
 end
